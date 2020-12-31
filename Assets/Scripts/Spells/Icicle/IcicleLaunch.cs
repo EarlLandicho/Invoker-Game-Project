1ded8929b7c0f24c5a19f91024f4fb5b23295508
Earl Landicho <earldaniellandicho@gmail.com>
2020-12-31 14:26:36 -0600

    Icicle fix

---
+++ Assets/Scripts/Spells/Icicle/IcicleLaunch.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IcicleLaunch : MonoBehaviour
+{
+    [SerializeField] private float speed = 0;
+
+    private Rigidbody2D rb;
+    private PlayerJump playerJump;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerJump = GameObject.Find("Player").GetComponent<PlayerJump>();
+    }
+
+    private void Start()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.velocity = transform.up * speed;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            rb.velocity = -transform.up * speed;
+
+            playerJump.Jump();
+        }
+        else
+        {
+            rb.velocity = transform.right * speed;
+        }
+
+    }
+}
