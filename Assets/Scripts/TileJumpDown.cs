25e368b5a19f3deee5f45a81d13ca825ddc8fe51
Earl Landicho <earldaniellandicho@gmail.com>
2021-01-16 15:42:00 -0600

    Implemented ledge jump down

---
+++ Assets/Scripts/TileJumpDown.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class TileJumpDown : MonoBehaviour
+{
+
+    private PlatformEffector2D effector;
+    private float reverseCollisionTimer = .1f;
+
+    private float reverseCollisionTimerTemp;
+    private bool reverseDelay;
+
+    void Awake()
+    {
+        effector = GetComponent<PlatformEffector2D>();
+        reverseCollisionTimerTemp = reverseCollisionTimer;
+    }
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.S))
+        {
+            effector.rotationalOffset = 180f;
+
+        }
+        else if(Input.GetKeyUp(KeyCode.S))
+        {
+            reverseDelay = true;
+
+        }
+
+
+        if(reverseDelay)
+        {
+            if (reverseCollisionTimerTemp > 0)
+            {
+                reverseCollisionTimerTemp -= Time.deltaTime;
+            }
+            else
+            {
+                reverseCollisionTimerTemp = reverseCollisionTimer;
+                effector.rotationalOffset = 0f;
+                reverseDelay = false;
+
+            }
+        }
+        
+    }
+
+
+
+
+
+}
