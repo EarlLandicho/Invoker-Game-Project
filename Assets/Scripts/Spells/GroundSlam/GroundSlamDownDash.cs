85951834a87a8a07d2f4fa907a05283c2593cec0
Earl Landicho <earldaniellandicho@gmail.com>
2021-02-06 10:49:55 -0600

    Added sprites and animations

--- Assets/Scripts/Spells/GroundSlam/GroundSlamDownDash.cs
+++ Assets/Scripts/Spells/GroundSlam/GroundSlamDownDash.cs
@@ -7,10 +7,14 @@ public class GroundSlamDownDash : MonoBehaviour
     [SerializeField]
     private float forceY = 0;
     private Rigidbody2D playerRigidBody;
+    private PlayerJump playerJump;
+
+    private bool wasGrounded;
 
     void Awake()
     {
         playerRigidBody = GameObject.Find("Player").GetComponent<Rigidbody2D>();
+        playerJump = GameObject.Find("Player").GetComponent<PlayerJump>();
 
         playerRigidBody.AddForce(new Vector2(0, forceY));
 
