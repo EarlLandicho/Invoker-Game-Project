7101ff20c987cb211c195d146bcf0d9f3d13e3d5
Earl Landicho <earldaniellandicho@gmail.com>
2021-02-22 10:04:14 -0600

    Updating combo bar

--- Assets/Scripts/ComboBar/ComboBarUI.cs
+++ Assets/Scripts/ComboBar/ComboBarUI.cs
@@ -2,19 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+
 
 public class ComboBarUI : MonoBehaviour
 {
     private TextMeshProUGUI comboBarText;
+    private Slider slider;
 
     void Awake()
     {
+        slider = GetComponent<Slider>();
         comboBarText = GetComponent<TextMeshProUGUI>();
     }
 
     void LateUpdate()
     {
         comboBarText.text = ComboBar.currentBarLevel.ToString();
+        slider.value = ComboBar.currentBarLevel;
     }
 
 }
