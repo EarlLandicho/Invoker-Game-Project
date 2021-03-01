39160cb7b161d8d00370b6299cd339c830fc1b54
Earl Landicho <earldaniellandicho@gmail.com>
2021-03-01 11:23:30 -0600

    Updated SLL Solar Flare animation

--- Assets/Scripts/Spells/SolarFlare/SolarFlare.cs
+++ Assets/Scripts/Spells/SolarFlare/SolarFlare.cs
@@ -3,37 +3,48 @@ using UnityEngine;
 
 public class SolarFlare : MonoBehaviour
 {
-    [SerializeField] private float radius = 0;
+    [SerializeField] private float projectileDestroyRadius = 0;
+    [SerializeField] private float enemyBurnRadius = 0;
 
-    private void Awake()
+    private void Start()
     {
-        Collider2D[] enemies = Physics2D.OverlapCircleAll(transform.position, radius, 1 << LayerMask.NameToLayer("Enemy"));
+        Collider2D[] enemies = Physics2D.OverlapCircleAll(transform.position, enemyBurnRadius, 1 << LayerMask.NameToLayer("Enemy"));
         foreach (Collider2D enemy in enemies)
         {
             enemy.gameObject.GetComponent<StatusEffect>().BecomeBurned();
         }
 
-        StartCoroutine("Flash");
-    }
+        //StartCoroutine("Flash");
+        LeanTween.value(gameObject, 1f, 0, .8f).setEaseInOutSine().setOnUpdate((float value) =>
+        {
+            SpriteRenderer spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+            Color newColor = spriteRenderer.color;
+            newColor.a = value;
+            spriteRenderer.color = newColor;
+        }).setOnComplete(OnComplete);
 
-    private IEnumerator Flash()
-    {
-        yield return new WaitForSeconds(.1f);
-
-        Collider2D[] projectiles = Physics2D.OverlapCircleAll(transform.position, radius, 1 << LayerMask.NameToLayer("Enemy Projectile"));
+        Collider2D[] projectiles = Physics2D.OverlapCircleAll(transform.position, projectileDestroyRadius, 1 << LayerMask.NameToLayer("Enemy Projectile"));
         foreach (Collider2D projectile in projectiles)
         {
             projectile.gameObject.GetComponent<EnemyProjectile>().DestroyWithAnimation();
-            //Destroy(projectile.gameObject);
         }
 
+    }
+
+    private void OnComplete()
+    {
         Destroy(gameObject);
     }
+
+
 
     private void OnDrawGizmos()
     {
         // Display the explosion radius when selected
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, radius);
+        Gizmos.DrawWireSphere(transform.position, projectileDestroyRadius);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, enemyBurnRadius);
     }
 }
