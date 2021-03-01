3973bfe74bdbf088b5aebee33353feb5e58c2afd
Earl Landicho <earldaniellandicho@gmail.com>
2021-03-01 10:41:13 -0600

    Updated SLG Reseter art and animation

--- Assets/Scripts/Spells/Reseter/Reseter.cs
+++ Assets/Scripts/Spells/Reseter/Reseter.cs
@@ -2,12 +2,20 @@
 
 public class Reseter : MonoBehaviour
 {
+    [SerializeField] private GameObject reseterAnimation;
     private SpiritCast spiritCast;
 
     private void Awake()
     {
         spiritCast = (SpiritCast)GameObject.FindObjectOfType(typeof(SpiritCast));
 
+        // This must be in Awake to work
         spiritCast.ResetAllCooldowns();
+    }
+
+    private void Start()
+    {
+        Instantiate(reseterAnimation, transform.position, transform.rotation);
+        Destroy(gameObject);
     }
 }
