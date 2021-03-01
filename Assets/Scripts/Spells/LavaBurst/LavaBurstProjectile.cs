d25a26762884a7db1d1cbc91d0e3845ce549e61a
Earl Landicho <earldaniellandicho@gmail.com>
2021-02-28 18:54:17 -0600

    Updated SSL Lava Burst animation and art

--- Assets/Scripts/Spells/LavaBurst/LavaBurstProjectile.cs
+++ Assets/Scripts/Spells/LavaBurst/LavaBurstProjectile.cs
@@ -4,17 +4,29 @@ public class LavaBurstProjectile : MonoBehaviour
 {
     [SerializeField] private float damage = 0;
 
+    [SerializeField] private GameObject explosionAnimation;
+
+
+
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.layer == LayerMask.NameToLayer("Enemy"))
         {
             col.gameObject.GetComponent<IHealth>().TakeDamage(damage);
             col.gameObject.GetComponent<StatusEffect>().BecomeBurned();
+
+            Explode();
         }
 
         if (col.gameObject.layer == LayerMask.NameToLayer("Ground"))
         {
-            Destroy(gameObject);
+            Explode();
         }
+    }
+
+    private void Explode()
+    {
+        Instantiate(explosionAnimation, transform.position, transform.rotation);
+        Destroy(gameObject);
     }
 }
