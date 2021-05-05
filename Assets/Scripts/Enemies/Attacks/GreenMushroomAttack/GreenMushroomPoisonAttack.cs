9afb41f9aca2938b8fec1739458ef2c6ab3db6f7
Earl Landicho <earldaniellandicho@gmail.com>
2021-05-05 18:11:37 -0500

    Started PoisonMushroom

---
+++ Assets/Scripts/Enemies/Attacks/GreenMushroomAttack/GreenMushroomPoisonAttack.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreenMushroomPoisonAttack : ProjectileAttack
+{
+    /*
+     * number of projectiles
+     * spawning length
+     * has to be attack animating while spawning projectiles
+     * max distance from spawn point for x and y
+     * projectile duration
+     * random time for lean tween to control speed
+     * set stunned animation in animator controller
+     * Set spawned animation
+     */
+}
